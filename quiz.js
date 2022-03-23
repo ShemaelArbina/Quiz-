54218bc342a70461b60467e440affc636a8f8a45
Arbina shemael <67178144+ShemaelArbina@users.noreply.github.com>
2022-03-22 20:52:29 -0400

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,34 @@
+﻿const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+
+function checkAnswer() {
+    /*event.preventDefault();*/
+    inputs = document.querySelectorAll("input[type=radio]:checked");
+    let score = 0;
+    for (let i = 0; i < inputs.length; i++) {
+        
+            if (inputs[i].value === "B") {
+                score += 25;
+            }
+
+            
+        }
+    console.log(score);
+    scrollTo(0, 0);
+    result.querySelector('span').textContent =`${score}%`;
+    result.classList.remove('d-none');
+    }
+
+
+
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        checkAnswer();
+       
+
+    })
+
+
+
